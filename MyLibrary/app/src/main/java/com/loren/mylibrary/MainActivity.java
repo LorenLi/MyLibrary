2ebc9748df5640009261721b32f1a2651eebfb22
lei.li <lei.li@jwaoo.com>
2017-04-24 15:51:17 +0800

    添加Toast工具.

--- MyLibrary/app/src/main/java/com/loren/mylibrary/MainActivity.java
+++ MyLibrary/app/src/main/java/com/loren/mylibrary/MainActivity.java
@@ -3,11 +3,15 @@ package com.loren.mylibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.loren.textlibrary.util.NetworkUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NetworkUtils.isNetworkAvailable(this);
+
     }
 }
