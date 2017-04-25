98500564fa28689103c5af5ba05a2ef69861c519
lei.li <lei.li@jwaoo.com>
2017-04-25 13:41:12 +0800

    去除ToastUtil多余代码.

---
+++ MyLibrary/textlibrary/src/main/java/com/loren/textlibrary/util/ToastUtils.java
@@ -0,0 +1,42 @@
+package com.loren.textlibrary.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * @author: Loren
+ * @create: 2016-7-5上午10:23:14
+ */
+public class ToastUtils {
+    private static Toast toast = null; //Toast的对象！
+
+    /**
+     * 多个覆盖式
+     * @param context
+     * @param id
+     */
+    public static void showToast(Context context, int id) {
+        if (toast == null) {
+            toast = Toast.makeText(context, id, Toast.LENGTH_SHORT);
+        }
+        else {
+            toast.setText(id);
+        }
+        toast.show();
+    }
+
+    /**
+     * 多个覆盖式
+     * @param context
+     * @param msg
+     */
+    public static void showToast(Context context,String msg){
+        if (toast == null) {
+            toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+        }
+        else {
+            toast.setText(msg);
+        }
+        toast.show();
+    }
+}
