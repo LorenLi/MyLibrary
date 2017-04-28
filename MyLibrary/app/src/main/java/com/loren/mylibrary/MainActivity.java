b942d463e5a2b6444a7cb8b3512adad13963b613
lei.li <lei.li@jwaoo.com>
2017-04-28 14:50:24 +0800

    删除以library形式的LogUtils

--- MyLibrary/app/src/main/java/com/loren/mylibrary/MainActivity.java
+++ MyLibrary/app/src/main/java/com/loren/mylibrary/MainActivity.java
@@ -3,8 +3,6 @@ package com.loren.mylibrary;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.loren.mylibrary.text.MyLogUtils;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyLogUtils.d("message");
-        MyLogUtils.d("MainActivity","messageH");
+//        MyLogUtils.d("message");
+//        MyLogUtils.d("MainActivity","messageH");
 
     }
 }
