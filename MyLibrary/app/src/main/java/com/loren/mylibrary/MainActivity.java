50e0ac33b0dcc4ed4c65cc4524408fb8da5b57e3
lei.li <lei.li@jwaoo.com>
2017-05-02 16:26:33 +0800

    add digestutils

--- MyLibrary/app/src/main/java/com/loren/mylibrary/MainActivity.java
+++ MyLibrary/app/src/main/java/com/loren/mylibrary/MainActivity.java
@@ -1,14 +1,33 @@
 package com.loren.mylibrary;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.loren.mylibrary.util.LoopTimer;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.tv);
+
+        Handler handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+
+        LoopTimer loopTimer = new LoopTimer(handler,runnable,100000);
+
+        loopTimer.start();
 
 
 
